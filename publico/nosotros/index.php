7d878b95006bf68d2ee5bf860fc2b598351862db
RollRoyce <itzalucard5@gmail.com>
2018-02-16 07:41:11 -0800

    chingadasos conmigo >:v

--- publico/nosotros/index.php
+++ publico/nosotros/index.php
@@ -29,7 +29,7 @@
         <?php include("../components/modalterminos.php") ?>
 
     <!-- Header -->
-		<header class="font-cover" id="">
+		<header class="font-cover" id="bv">
 			<h1>LUHO <br> <span>[Que bueno es que podamos conocernos]</span></h1>
 		</header>
 
