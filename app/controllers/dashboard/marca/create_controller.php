b82e247723256747bc20943bb91d3aeef2e949f5
RollRoyce <itzalucard5@gmail.com>
2018-04-15 09:58:27 -0700

    :$

--- app/controllers/dashboard/marca/create_controller.php
+++ app/controllers/dashboard/marca/create_controller.php
@@ -2,7 +2,7 @@
 require_once("../../app/models/marca.class.php");
 try{
     $marca = new Marca;
-    if(isset($_POST['insertar'])){
+    if(isset($_POST['crear'])){
         $_POST = $marca->validateForm($_POST);
         if($marca->setNombre($_POST['Nombre'])){ 
             if($marca->createMarca()){
@@ -18,5 +18,5 @@ try{
 }catch(Exception $error){
     Page::showMessage(2, $error->getMessage(), null);
 }
-require_once("../../app/views/dashboard/marcaymaterial/create_view.php");
+require_once("../../app/views/dashboard/marca/create_view.php");
 ?>
--- dashboard/marcymat/delete.php
+++ dashboard/marcymat/delete.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../../app/views/dashboard/templates/page.class.php");
-Page::templateHeader("Nosotros");
+Page::templateHeader("Marca");
 require_once("../../app/views/dashboard/sections/ocasionview.php");
-require_once("../../app/controllers/dashboard/marca/index_controller.php");
+require_once("../../app/controllers/dashboard/marca/delete_controller.php");
 Page::templateFooter();
 ?>
