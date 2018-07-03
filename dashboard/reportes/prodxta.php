67e7f9b11f606e5c1aa44ab27741448d6749271d
RollRoyce <itzalucard5@gmail.com>
2018-07-02 18:27:21 -0700

    kkk

--- dashboard/reportes/prodxta.php
+++ dashboard/reportes/prodxta.php
@@ -104,7 +104,7 @@ $pdf->Write( 6, "Fecha y Hora: ");
 $pdf->SetFont( 'Arial', '', 12 );
 $pdf->Write( 6, $time);
 $pdf->Ln( 12 );
-$pdf->Write( 6, "Despite the economic downturn, WidgetCo had a strong year. Sales of the HyperWidget in particular exceeded expectations. The fourth quarter was generally the best performing; this was most likely due to our increased ad spend in Q3." );
+$pdf->Write( 6, "En el siguiente reporte se muestran a continuacion los datos de los productos existentes en la categoria de: $Titulo." );
 
 
 /**
